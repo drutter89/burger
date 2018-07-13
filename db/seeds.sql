d91ada6837cbd3cac11d9f5901eec049846f3e06
Dylan Rutter <dylanrutter@Dylans-MBP.attlocal.net>
2018-07-13 04:05:22 -0500

    starting files

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO burgers
+  (burger_name, devoured)
+VALUES
+  ('cheese_burger', 'False');
+INSERT INTO burgers
+  (burger_name, devoured)
+VALUES
+  ('avocado_burger', 'True');
+INSERT INTO burgers
+  (burger_name, devoured)
+VALUES
+  ('vegan_burger', 'False');
+
+
